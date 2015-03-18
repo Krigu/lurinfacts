e9f18c1e84671ea96e3b4afd78ab160b6a134b49
krigu <chris.feuz@gmail.com>
2015-03-18 19:17:03 +0100

    Added markerFactory

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -9,21 +9,28 @@
  */
 (function () {
     angular.module('lurinfacts')
-        .controller('MapCtrl', ['$scope', 'uiGmapGoogleMapApi',
-            '$interval', '$timeout', 'uiGmapGmapUtil',
-            function ($scope, uiGmapGoogleMapApi, $interval, $timeout, uiGmapGmapUtil) {
-                this.map = {};
-                var self = this;
-                uiGmapGoogleMapApi.then(function (maps) {
-                    angular.extend(self.map, {
-                        center: {
-                            latitude: 42.194576,
-                            longitude: -122.709477
-                        },
-                        zoom: 13,
-                        control: {}
-                    });
+        .controller('MapCtrl', ['$scope', 'markerFactory', 'uiGmapGoogleMapApi', function ($scope, markerFactory, uiGmapGoogleMapApi) {
 
+            var self = this;
+
+            this.map = {};
+
+            this.markers = {}
+
+            markerFactory.getMarkers().success(function(data){
+                self.markers=data;
+            });
+
+            uiGmapGoogleMapApi.then(function (maps) {
+                angular.extend(self.map, {
+                    center: {
+                        latitude: 26.949573,
+                        longitude: 7.446969
+                    },
+                    zoom: 3,
+                    control: {}
                 });
-            }])
+
+            });
+        }])
 })();
