f4a0ed21385f8e5609c082283200c46309491d85
krigu <chris.feuz@gmail.com>
2015-03-18 23:32:07 +0100

    Added window for each location in map

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -11,18 +11,18 @@
     angular.module('lurinfacts')
         .controller('MapCtrl', ['$scope', 'markerFactory', 'uiGmapGoogleMapApi', function ($scope, markerFactory, uiGmapGoogleMapApi) {
 
-            var self = this;
+            $scope.map = {};
 
-            this.map = {};
+            $scope.markers = {}
 
-            this.markers = {}
+            $scope.selectedMarker = {};
 
-            markerFactory.getMarkers().success(function(data){
-                self.markers=data;
+            markerFactory.getMarkers().success(function (data) {
+                $scope.markers = data;
             });
 
             uiGmapGoogleMapApi.then(function (maps) {
-                angular.extend(self.map, {
+                angular.extend($scope.map, {
                     center: {
                         latitude: 26.949573,
                         longitude: 7.446969
@@ -32,5 +32,20 @@
                 });
 
             });
+
+            $scope.windowOptions = {
+                show: false
+            };
+
+            $scope.onClick = function (marker) {
+                $scope.selectedMarker = marker.model;
+                $scope.windowOptions.show = !$scope.windowOptions.show;
+            };
+
+            $scope.closeClick = function () {
+                $scope.windowOptions.show = false;
+            };
+
+
         }])
 })();
