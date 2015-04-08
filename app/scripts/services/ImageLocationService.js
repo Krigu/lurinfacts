ee01698c462c6a038f54d11c0f3b32e04ac72bdc
Stef Chäser <info.stef.kaeser@gmail.com>
2015-04-08 22:17:01 +0200

    first draft of the admin gui for adding image locations

---
+++ app/scripts/services/ImageLocationService.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Käser on 07/04/2015.
+ */
+angular.module('lurinfacts').factory('ImageLocationService', function ($q,$firebaseArray) {
+  var firebaseRef = 'https://burning-inferno-892.firebaseio.com/';
+
+  var saveLocation = function (metaData, image, thumbnail) {
+    var uuid = guid();
+
+    return $q.all([
+      saveMetadata(uuid, metaData,thumbnail),
+      saveImage(uuid, image)
+      //saveThumbnail(uuid, thumbnail)
+    ])
+  };
+
+  var guid = function(){
+    return uuid = (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+  };
+
+  var S4 = function() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  };
+
+  var saveMetadata = function (guid, metadata,thumbnail) {
+    var d = $q.defer();
+    metadata.guid = guid;
+    metadata.thumbnail = thumbnail;
+    var firebaseMetaData = new Firebase(firebaseRef + 'imageLocation/' + guid);
+    firebaseMetaData.set(metadata, function () {
+      console.log('metadata uploaded under hash:' + guid);
+      d.resolve(guid);
+    });
+    return d.promise;
+  };
+
+  var saveImage = function (guid, image) {
+    var d = $q.defer();
+    var firebasePicture = new Firebase(firebaseRef + 'image/' + guid + '/filePayload');
+    firebasePicture.set(image, function () {
+      console.log('image uploaded under hash:' + guid);
+      d.resolve(guid);
+    });
+    return d.promise;
+  };
+
+  //var saveThumbnail = function (guid, thumbnail) {
+  //  var d = $q.defer();
+  //  var firebaseThumbnail = new Firebase(firebaseRef + 'thumbnail/' + guid + '/filePayload');
+  //  firebaseThumbnail.set(thumbnail, function () {
+  //    console.log('thumbnail uploaded under hash:' + guid);
+  //    d.resolve(guid);
+  //  });
+  //  return d.promise;
+  //};
+
+  var locationsAsFirebaseArray = function(){
+    // Get a reference to our posts
+    var ref = new Firebase(firebaseRef + 'imageLocation/');
+    return $firebaseArray(ref);
+  };
+
+
+  return {
+    saveLocation: saveLocation,
+    locationsAsFirebaseArray : locationsAsFirebaseArray
+  };
+});
