424e160654a08785f512ab3a543c5846ff067061
Stef Chäser <info.stef.kaeser@gmail.com>
2015-04-26 15:13:45 +0200

    add login to menu container

--- app/scripts/directive/loginDirective.js
+++ app/scripts/directive/loginDirective.js
@@ -4,20 +4,25 @@ angular.module('lurinfacts').directive('fireBaseLogin', function ($sce, LoginSer
     return {
         restrict: 'EA',
         replace: true,
-        template: '<div><div ng-hide="isLoggedIn">User:<input type="text" ng-model="user" /> ' +
-            'Password: <input type="password" ng-model="password" />' +
-            '<input type="button" class="btn" value="OK" data-ng-click="login()"></div><div ng-show="isLoggedIn"><input class="btn" type="button" data-ng-click="logout()" value="Logout" /></div></div>',
+        templateUrl: './../../views/loginDirective.html',
         link: function (scope) {
-            scope.isLoggedIn = LoginService.isUserLoggedIn();
+            scope.showLogin = false;
+            scope.isLoggedIn = LoginService.isUserLoggedIn;
             scope.$watch(LoginService.isUserLoggedIn, function (newVal) {
                 console.log('login status changed: user logged in' + newVal);
                 scope.isLoggedIn = newVal;
             });
+            scope.loginCallback = function (isLoggedInNow) {
+                console.log('callbacked isLoggedInNow: ' + isLoggedInNow);
+                scope.isLoggedIn = isLoggedInNow;
+                scope.$apply();
+            };
 
             scope.login = function () {
                 console.log('login for: ' + scope.user);
-                LoginService.doUserLogin(scope.user, scope.password, true);
+                LoginService.doUserLogin(scope.user, scope.password, true, scope.loginCallback);
             };
+
             scope.logout = function () {
                 console.log('logout');
                 LoginService.logout();
--- app/scripts/services/LoginService.js
+++ app/scripts/services/LoginService.js
@@ -1,46 +1,51 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('lurinfacts').factory('LoginService', function () {
-    var UserIsLoggedIn = false;
-    var firebaseRef = 'https://burning-inferno-892.firebaseio.com/';
-    var ref = new Firebase(firebaseRef);
-    var authData = ref.getAuth();
-    if (authData) {
-        UserIsLoggedIn = true;
-        console.log('User ' + authData.uid + ' is logged in with ' + authData.provider);
-    } else {
-        console.log('User is logged out');
-        UserIsLoggedIn = false;
-    }
+    angular.module('lurinfacts').factory('LoginService', function () {
+        var UserIsLoggedIn = false;
+        var firebaseRef = 'https://burning-inferno-892.firebaseio.com/';
+        var ref = new Firebase(firebaseRef);
+        var authData = ref.getAuth();
+        if (authData) {
+            UserIsLoggedIn = true;
+            console.log('User ' + authData.uid + ' is logged in with ' + authData.provider);
+        } else {
+            console.log('User is logged out');
+            UserIsLoggedIn = false;
+        }
 
-    var isUserLoggedIn = function () {
-        return UserIsLoggedIn;
-    };
+        var isUserLoggedIn = function () {
+            return UserIsLoggedIn;
+        };
 
-    var doUserLogin = function (email, password, iAmLazy) {
-        ref.authWithPassword({
-            email: email,
-            password: password,
-            remember: iAmLazy
-        }, function (error, authData) {
-            if (error) {
-                console.log('Login Failed!', error);
-                UserIsLoggedIn = false;
-            } else {
-                console.log('Authenticated successfully with payload:', authData);
-                UserIsLoggedIn = true;
-            }
-        });
-    };
+        var doUserLogin = function (email, password, iAmLazy, callback) {
+            ref.authWithPassword({
+                email: email,
+                password: password,
+                remember: iAmLazy
+            }, function (error, authData) {
+                if (error) {
+                    console.log('Login Failed!', error);
+                    UserIsLoggedIn = false;
+                } else {
+                    console.log('Authenticated successfully with payload:', authData);
+                    UserIsLoggedIn = true;
+                }
+                if (callback) {
+                    callback(UserIsLoggedIn);
+                }
+            });
+        };
 
-    var logout = function () {
-        ref.unauth();
-        UserIsLoggedIn = false;
-    };
+        var logout = function () {
+            ref.unauth();
+            UserIsLoggedIn = false;
+        };
 
-    return {
-        doUserLogin: doUserLogin,
-        logout: logout,
-        isUserLoggedIn: isUserLoggedIn
-    };
-});
+        return {
+            doUserLogin: doUserLogin,
+            logout: logout,
+            isUserLoggedIn: isUserLoggedIn
+        };
+    });
+}());
