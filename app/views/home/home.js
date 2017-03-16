edf0e554ab98e1487021aab3f63f49bb1bf62bad
Stef Käser <info.stef.kaeser@gmail.com>
2017-03-16 22:03:10 +0100

    remove arrow functions, (doesn't work with gulp-uglify)

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -17,38 +17,38 @@
                 $scope.locations = [];
 
                 var FetchAll = function () {
-                        return dbPromise.then(db => {
+                        return dbPromise.then(function (db) {
                                 return db.transaction('latestimages')
                                         .objectStore('latestimages').getAll();
                         });
                 };
 
-                FetchAll().then(allObjs => {
+                FetchAll().then(function (allObjs) {
                         console.log(allObjs);
-                        allObjs.map(x => $scope.locations.unshift(x));
+                        allObjs.map(function (x) { $scope.locations.unshift(x) });
                 });
 
                 var DeleteOldEntries = function () {
                         FetchAll().then(function (all) {
                                 all
-                                        .sort((a, b) => b.inserted.getTime() - a.inserted.getTime())
-                                        .filter((x, idx) => idx >= 2)
+                                        .sort(function (a, b) { return b.inserted.getTime() - a.inserted.getTime() })
+                                        .filter(function (x, idx) { return idx >= 2 })
                                         .map(DeleteOldEntry);
                         })
                 };
 
                 var DeleteOldEntry = function (img) {
                         console.log('delete entry from indexedDB ' + img.imageTitle);
-                        dbPromise.then(db => {
+                        dbPromise.then(function (db) {
                                 return db.transaction('latestimages', 'readwrite')
                                         .objectStore('latestimages').delete(img.imageKey);
-                        }).then(obj => console.log(obj));
+                        }).then(function (obj) { console.log(obj) });
                 };
 
                 var AddImageToCache = function (img) {
                         img.inserted = new Date();
-                        console.log('add entry to indexedDB ' + img.imageTitle,img.inserted);
-                        dbPromise.then(db => {
+                        console.log('add entry to indexedDB ' + img.imageTitle, img.inserted);
+                        dbPromise.then(function (db) {
                                 const tx = db.transaction('latestimages', 'readwrite');
                                 tx.objectStore('latestimages').put(img, img.imageKey);
                                 return tx.complete;
@@ -57,7 +57,7 @@
 
                 ImageLocationService.latestLocation(2).on("child_added", function (snapshot) {
                         //not added yet
-                        if (!$scope.locations.filter(x => x.imageKey == snapshot.val().imageKey).length) {
+                        if (!$scope.locations.filter(function (x) { return x.imageKey == snapshot.val().imageKey }).length) {
                                 $scope.locations.unshift(snapshot.val());
                         }
                         AddImageToCache(snapshot.val());
