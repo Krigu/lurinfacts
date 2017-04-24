b5828be5be6e2c7aa82375153688b1c449720b09
Stef Käser <info.stef.kaeser@gmail.com>
2017-04-24 22:23:09 +0200

    add sw-toolbox

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       '../bower_components/js-rich-marker/src/richmarker.js',
       '../bower_components/angular-google-maps/dist/angular-google-maps.js',
       '../bower_components/angular-route/angular-route.js',
+      '../bower_components/sw-toolbox/sw-toolbox.js',
       // endbower
       '../app/scripts/**/*.js',
       //'../test/mock/**/*.js',
