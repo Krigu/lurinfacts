68d35b085d4837119dd654d2fe9610503c5c4128
Stef Käser <info.stef.kaeser@gmail.com>
2017-05-03 18:30:20 +0200

    improve image resizer

--- app/scripts/services/ImageResizeService.js
+++ app/scripts/services/ImageResizeService.js
@@ -20,42 +20,17 @@ angular.module('lurinfacts').factory('ImageResizeService', function ($q) {
     var resizeLoadedImage = function (image, size) {
       var mainCanvas = document.createElement('canvas');
       //portrait or landscape image?
-      mainCanvas.width = image.width < image.height ?  768 : 1024;
-      mainCanvas.height = image.width > image.height ?  768 : 1024;
+      mainCanvas.width = image.width;
+      mainCanvas.height = image.height;
       var ctx = mainCanvas.getContext('2d');
       ctx.drawImage(image, 0, 0, mainCanvas.width, mainCanvas.height);
       //size = parseInt($('#size').get(0).value, 10);
-      while (mainCanvas.width > size) {
+      while (mainCanvas.width > size || mainCanvas.height > size) {
         mainCanvas = halfSize(mainCanvas);
       }
       return mainCanvas.toDataURL('image/jpeg');
     };
 
-    var reresizeImage = function (src) {
-      // perform some asynchronous operation, resolve or reject the promise when appropriate.
-      return $q(function (resolve, reject) {
-        var img = new Image();
-        img.onload = function () {
-          var resized = reresizeLoadedImage(img);
-          resolve(resized);
-        };
-        img.onerror = function (e) {
-          reject(e);
-        };
-        img.src = src;
-      });
-    };
-
-    var reresizeLoadedImage = function (image) {
-      var mainCanvas = document.createElement('canvas');
-      //portrait or landscape image?
-      mainCanvas.width = image.height;
-      mainCanvas.height = image.width;
-      var ctx = mainCanvas.getContext('2d');
-      ctx.drawImage(image, 0, 0, mainCanvas.width, mainCanvas.height);
-      //size = parseInt($('#size').get(0).value, 10);
-      return mainCanvas.toDataURL('image/jpeg');
-    };
     /*
      * Draw initial canvas on new canvas and half it's size
      */
@@ -70,7 +45,6 @@ angular.module('lurinfacts').factory('ImageResizeService', function ($q) {
 
     return {
       resize: resizeImage,
-      reresizeImage : reresizeImage
     };
 
   }
