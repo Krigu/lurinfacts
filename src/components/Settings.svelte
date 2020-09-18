62123f26b2b57b8a6a84d87c2c76f14d7b0c5b11
Stef Käser <info.stef.kaeser@gmail.com>
2020-09-18 21:21:48 +0200

    add option to clear cache / remove service worker

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -8,7 +8,8 @@
   import Button, { Label } from "@smui/button";
 
   let isWorking = false;
-  let isPushFeatured = "serviceWorker" in navigator && "PushManager" in window;
+  let hasServiceWorker = "serviceWorker" in navigator;
+  let isPushFeatured = hasServiceWorker && "PushManager" in window;
 
   async function togglePush(disable) {
     isWorking = true;
@@ -21,36 +22,80 @@
     notify(result.msg);
     isWorking = false;
   }
+
+  async function clearStorage() {
+    localStorage.clear();
+    if (!window.indexedDB) {
+      return;
+    }
+    console.log("delete indexedDB named lurinfacts_3_0");
+    window.indexedDB.deleteDatabase("lurinfacts_3_0");
+    notify("All cleared!");
+  }
+
+  function unregisterServiceWorker() {
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+        notify("ServiceWorker kicked!");
+      }
+    });
+  }
 </script>
 
 <style type="text/postcss">
-
+  .configItem {
+    padding-bottom: 60px;
+  }
 </style>
 
 <div class="contentpadding">
-  <h1>Settings</h1>
 
-  <h2>Push Notifications</h2>
-  <p>Get the latest and greatest news about lurins trips and wisdom.</p>
-  {#if isPushFeatured}
-    <Button
-      on:click={() => togglePush(true)}
-      disabled={isWorking}
-      variant="raised"
-      class="formButton">
-      <Label>Disable</Label>
+  <div class="configItem">
+    <h1>Settings</h1>
+
+    <h2>Push Notifications</h2>
+    <p>Get the latest and greatest news about lurins trips and wisdom.</p>
+    {#if isPushFeatured}
+      <Button
+        on:click={() => togglePush(true)}
+        disabled={isWorking}
+        variant="raised"
+        class="formButton">
+        <Label>Disable</Label>
+      </Button>
+      <Button
+        on:click={() => togglePush(false)}
+        disabled={isWorking}
+        variant="raised"
+        class="formButton">
+        <Label>Enable</Label>
+      </Button>
+    {:else}
+      <p>
+        Sorry, your browser doesn't feature push notification. Ask lurin how to
+        download a real browser
+      </p>
+    {/if}
+  </div>
+  <div class="configItem">
+
+    <h2>Cache</h2>
+    <p>Clear LocalStorage and IndexDB</p>
+    <Button on:click={() => clearStorage()} variant="raised" class="formButton">
+      <Label>Clear</Label>
     </Button>
-    <Button
-      on:click={() => togglePush(false)}
-      disabled={isWorking}
-      variant="raised"
-      class="formButton">
-      <Label>Enable</Label>
-    </Button>
-  {:else}
-    <p>
-      Sorry, your browser doesn't feature push notification. Ask lurin how to
-      download a real browser
-    </p>
-  {/if}
+  </div>
+  <div class="configItem">
+    {#if hasServiceWorker}
+      <h2>Service Worker</h2>
+      <Button
+        on:click={() => unregisterServiceWorker()}
+        variant="raised"
+        class="formButton">
+        <Label>Unregister Service Worker</Label>
+      </Button>
+    {/if}
+  </div>
+
 </div>
